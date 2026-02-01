84ed70965ff07dea4421f64ac00ed82397c550cd
David Miserak <David.Miserak@gmail.com>
2026-02-01 14:11:08 -0500

    chore: add day.js to localize times

--- children/forms.py
+++ children/forms.py
@@ -1,5 +1,6 @@
+from datetime import timedelta
+
 from django import forms
-from django.utils import timezone
 
 from .models import Child, DiaperChange, Nap
 
@@ -20,30 +21,66 @@ class ChildForm(forms.ModelForm):
         }
 
 
-class DiaperChangeForm(forms.ModelForm):
+class LocalDateTimeFormMixin:
+    """Mixin to handle local timezone input for datetime fields."""
+
+    datetime_field_name = None  # Subclasses must set this
+
+    tz_offset = forms.IntegerField(
+        widget=forms.HiddenInput(attrs={"class": "tz-offset"}),
+        required=False,
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        tz_offset = cleaned_data.get("tz_offset")
+        dt_value = cleaned_data.get(self.datetime_field_name)
+
+        if tz_offset is not None and dt_value is not None:
+            # tz_offset is in minutes, negative means behind UTC
+            # e.g., EST is -300 (5 hours behind)
+            # To convert local time to UTC, add the offset
+            cleaned_data[self.datetime_field_name] = dt_value + timedelta(
+                minutes=tz_offset
+            )
+
+        return cleaned_data
+
+
+class DiaperChangeForm(LocalDateTimeFormMixin, forms.ModelForm):
+    datetime_field_name = "changed_at"
+
+    tz_offset = forms.IntegerField(
+        widget=forms.HiddenInput(attrs={"class": "tz-offset"}),
+        required=False,
+    )
     changed_at = forms.DateTimeField(
-        initial=timezone.now,
         widget=forms.DateTimeInput(
-            attrs={"type": "datetime-local", "class": "form-control"},
+            attrs={"type": "datetime-local", "class": "form-control local-datetime"},
         ),
     )
 
     class Meta:
         model = DiaperChange
-        fields = ["change_type", "changed_at"]
+        fields = ["change_type", "changed_at", "tz_offset"]
         widgets = {
             "change_type": forms.Select(attrs={"class": "form-select"}),
         }
 
 
-class NapForm(forms.ModelForm):
+class NapForm(LocalDateTimeFormMixin, forms.ModelForm):
+    datetime_field_name = "napped_at"
+
+    tz_offset = forms.IntegerField(
+        widget=forms.HiddenInput(attrs={"class": "tz-offset"}),
+        required=False,
+    )
     napped_at = forms.DateTimeField(
-        initial=timezone.now,
         widget=forms.DateTimeInput(
-            attrs={"type": "datetime-local", "class": "form-control"},
+            attrs={"type": "datetime-local", "class": "form-control local-datetime"},
         ),
     )
 
     class Meta:
         model = Nap
-        fields = ["napped_at"]
+        fields = ["napped_at", "tz_offset"]
